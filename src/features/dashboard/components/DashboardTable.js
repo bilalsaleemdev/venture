dd45ff0df0466510d934ff39f88d2e4ddbd4db36
Ali07117 <alirashid07117@gmail.com>
2024-03-30 05:50:02 +0500

    update

--- src/features/dashboard/components/DashboardTable.js
+++ src/features/dashboard/components/DashboardTable.js
@@ -21,7 +21,7 @@ const TopSideButtons = () => {
 
 
 const TEAM_MEMBERS = [
-    {name : "Alex", avatar : "https://reqres.in/img/faces/1-image.jpg", email : "alex@dashwind.com", role : "Owner", joinedOn : moment(new Date()).add(-5*1, 'days').format("DD MMM YYYY"), lastActive : "5 hr ago"},
+    {name : "Alex", avatar : "https://reqres.in/img/faces/1-image.jpg", email : "alex@dashwind.com", role : " Owner", joinedOn : moment(new Date()).add(-5*1, 'days').format("DD MMM YYYY"), lastActive : "5 hr ago"},
     {name : "Ereena", avatar : "https://reqres.in/img/faces/2-image.jpg", email : "ereena@dashwind.com", role : "Admin", joinedOn : moment(new Date()).add(-5*2, 'days').format("DD MMM YYYY"), lastActive : "15 min ago"},
     {name : "John", avatar : "https://reqres.in/img/faces/3-image.jpg", email : "jhon@dashwind.com", role : "Admin", joinedOn : moment(new Date()).add(-5*3, 'days').format("DD MMM YYYY"), lastActive : "20 hr ago"},
     {name : "Matrix", avatar : "https://reqres.in/img/faces/4-image.jpg", email : "matrix@dashwind.com", role : "Manager", joinedOn : moment(new Date()).add(-5*4, 'days').format("DD MMM YYYY"), lastActive : "1 hr ago"},
@@ -36,7 +36,7 @@ function DashboardTable(){
     const [members, setMembers] = useState(TEAM_MEMBERS)
 
     const getRoleComponent = (role) => {
-        if(role  === "Admin")return <div className="badge badge-secondary">{role}</div>
+        if(role  === "Aaaadmin")return <div className="badge badge-secondary">{role}</div>
         if(role  === "Manager")return <div className="badge">{role}</div>
         if(role  === "Owner")return <div className="badge badge-primary">{role}</div>
         if(role  === "Support")return <div className="badge badge-accent">{role}</div>
@@ -49,32 +49,32 @@ function DashboardTable(){
             <TitleCard title="Transaction Details" topMargin="mt-2" TopSideButtons={<TopSideButtons />}>
 
                 {/* Team Member list in table format loaded constant */}
-            <div className="overflow-x-auto w-full">
+            <div className="overflow-x-auto w-full ">
             
-                <table className="table w-full rounded-xl">
+                <table className="table w-full ">
                     <thead >
-                    <tr className="">
-                        <th className=" "> <input type="checkbox" name="" id="" /> Id</th>
-                        <th><span className="flex"><p className="text-[#727272]">Date</p> <img src="/images/dashboard-table-arrow.svg" alt="" /></span></th>
-                        <th><span className="flex"><p className="text-[#727272]">Client</p><img src="/images/dashboard-table-arrow.svg" alt="" /></span></th>
-                        <th><span className="flex"><p className="text-[#727272]">Company</p><img src="/images/dashboard-table-arrow.svg" alt="" /></span></th>
-                        <th><span className="flex"><p className="text-[#727272]">Payment</p><img src="/images/dashboard-table-arrow.svg" alt="" /></span></th>
-                        <th><span className="flex"><p className="text-[#727272]">Status</p><img src="/images/dashboard-table-arrow.svg" alt="" /></span></th>
-                        <th><span className="flex"><p className="text-[#727272]">Action</p><img src="/images/dashboard-table-arrow.svg" alt="" /></span></th>
+                    <tr className="h-[45px]">
+                        <th> <span className="flex"><input type="checkbox" name="" id="" /><p className="text-[#727272] ml-[8px]">Id</p></span></th>
+                        <th><span className="flex"><p className="text-[#727272] ml-[8px]">Date</p> <img src="/images/dashboard-table-arrow.svg" alt="" /></span></th>
+                        <th><span className="flex"><p className="text-[#727272] ml-[8px]">Client</p><img src="/images/dashboard-table-arrow.svg" alt="" /></span></th>
+                        <th><span className="flex"><p className="text-[#727272] ml-[8px]">Company</p><img src="/images/dashboard-table-arrow.svg" alt="" /></span></th>
+                        <th><span className="flex"><p className="text-[#727272] ml-[8px]">Payment</p><img src="/images/dashboard-table-arrow.svg" alt="" /></span></th>
+                        <th><span className="flex"><p className="text-[#727272] ml-[8px]">Status</p><img src="/images/dashboard-table-arrow.svg" alt="" /></span></th>
+                        <th><span className="flex"><p className="text-[#727272] ml-[8px]">Action</p><img src="/images/dashboard-table-arrow.svg" alt="" /></span></th>
                     </tr>
                     </thead>
-                    <tbody>
+                    <tbody className="border border-2">
                         {
                             members.map((l, k) => {
                                 return(
-                                    <tr key={k}>
-                                    <td><input type="checkbox" name="" id="" /> 1528902 </td>
-                                    <td className="flex"><img src="/images/dashboard-calender.svg" alt="" /> <p className="text-[#727272] font-medium">18/09/2016</p> </td>
+                                    <tr className="h-[48px]">
+                                    <td className=""><input type="checkbox" name="" id="" /> 1528902 </td>
+                                    <td><span className="flex"><img src="/images/dashboard-calender.svg" alt="" />  <p className="text-[#727272] font-medium">18/09/2016</p></span> </td>
                                     <td className="font-medium text-[#727272]">Leslie Alexander</td>
                                     <td className="font-medium text-[#727272]">Abstergo Ltd.</td>
                                     <td>$2.120</td>
-                                    <td>{getRoleComponent(l.role)}</td>
-                                    <td> <img src="/images/dashboard-table-3-dot.svg" alt="" /></td>
+                                    <td><span className=""><button className="rounded-[20px] bg-[#EEF5F0] px-[10px] text-[#589E67] flex items-center"><img src="/images/dashboard-table-button-dot.svg" alt="" className="mr-[8px]"/>  COMPELETED</button></span></td>
+                                    <td> <img src="/images/dashboard-table-3-dot.svg" alt="" className="cursor-pointer"/></td>
                                     </tr>
                                 )
                             })
